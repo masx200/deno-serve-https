febcbb79201d62f0cfb3e17207b766fe17cac559
masx200 <34191203+masx200@users.noreply.github.com>
2022-08-13 15:57:55 +0800

    Update mod.ts

--- mod.ts
+++ mod.ts
@@ -9,8 +9,3 @@ import { serve_https } from "./serve_https.ts";
 export { serve_http, serve_https };
 export type { ServeHttpInit };
 export type { ConnInfo, Handler, Handlers, ServeHttpsInit };
-// import { serve } from "https://deno.land/std@0.147.0/http/server.ts";
-// import { handler } from "https://deno.land/x/masx200_hello_world_deno_deploy@1.1.5/mod.ts";
-// import { parse } from "https://deno.land/std@0.147.0/flags/mod.ts";
-// if (import.meta.main) {
-//     await main();
